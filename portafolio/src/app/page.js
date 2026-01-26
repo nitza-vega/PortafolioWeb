cbb117d7fd374b6fbe5dc692967a8fbc396bba72
nitza-vega <nitza-vega@hotmail.com>
2026-01-25 19:52:47 -0700

    Button

--- portafolio/src/app/page.js
+++ portafolio/src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Button from "@/components/ui/display/Button";
+import Button from "@/components/atomic/Button";
 
 export default function Home() {
   return (
--- portafolio/src/components/atomic/Button.js
+++ portafolio/src/components/atomic/Button.js
@@ -1,21 +1,45 @@
 import React from "react";
 
 export default function Button({
-    variant = "primary",
+    text,
+    hasIcon,
+    icon, 
+    variant = "secondary",
     disabled = false,
     children,
     onClick
 }) {
     return(
         <>
-            <button className={`${variant} ${disabled ? "disabled" : ""}`} onClick={disabled ? undefined : onClick}>
+            <button className={`button ${variant} ${disabled ? "disabled" : ""}`} onClick={disabled ? undefined : onClick}>
+                {hasIcon && <span className="icon">{ icon }</span>}
+                {text && <span>{ text }</span>}
                 {children}
             </button>
-            <style>
-                button {
 
+            <style>{`
+                button {
+                    padding: 10px 18px;
+                    border-radius: 100px;
+                    cursor: pointer;
+                    transition: transform 0.2s ease;
                 }
-            </style>
+
+                .button:hover {
+                    transform: scale(1.05);
+                }
+
+                .primary {
+                    background: linear-gradient(to bottom, #8732CD, #B27CDF, #C8A1E8);
+                }
+
+                .secondary {
+                    background: none;
+                    border: 3px solid;
+                    border-radius: 100px;
+                    border-image: linear-gradient(to bottom, #8732CD, #B27CDF, #C8A1E8) 1;
+                }
+            `}</style>
         </>
     )
 }
